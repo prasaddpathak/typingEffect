8d5c4699f74310922b30e00a3b701740bb103ede
Prasad Pathak <prasaddpathak@gmail.com>
2020-12-30 12:10:21 +0530

    version 2.0

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const texts = ["websites", "illustrations", "blockchains"];
-let count = 0;
-let index = 0;
-let currentText = '';
-let letter = '';
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
 
-(function type(){
+const textArray = ["hard.", "fun.", "a journey.", "LIFE!"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
 
-    if(count === texts.length){
-        count = 0;
-    }
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
 
-    currentText = texts[count];
-    letter = currentText.slice(0,++index);
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
 
-    document.querySelector('.typing').textContent = letter;
-    
-    if (letter.length === currentText.length) {
-        
-        // (function unType(){
-            
-        //     letter = currentText.slice(0,index--);
-        //     document.querySelector('.typing').textContent = letter;
-            
-        //     if (index === 0) {
-        //         continue;
-        //     }
-
-        //     setTimeout(unType,400);
-
-        // }());
-
-        count++;
-        index = 0;
-    }
-    setTimeout(type,400);
-    
-}());
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 This is basic html/css/js template to build typing effect in HTML.
+
+Preview : https://condescending-almeida-593531.netlify.app/
